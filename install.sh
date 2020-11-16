bc9e2dc3ed43a1d0fc7721fb4f124c7938ad2bf9
向上的恩赐 <19582627+stdec@users.noreply.github.com>
2020-11-16 14:39:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-#安装Docker
-#安装依赖包
 yum -y install yum-utils device-mapper-persistent-data lvm2
-#添加yum仓库
 yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-# 安装指定版本的Docker-CE:
-# yum list docker-ce.x86_64 --showduplicates | sort -r
-#安装docker
+yum list docker-ce.x86_64 --showduplicates | sort -r
+
 yum install -y docker-ce-19.03.5 docker-ce-cli-19.03.5 containerd.io
 
-#创建docker配置文件
+
 mkdir -p /etc/docker
 cat > /etc/docker/daemon.json <<EOF 
 {
@@ -30,10 +26,8 @@ EOF
 
 sysctl -p
 
-#创建docker目录
 mkdir -p /data/docker
 
-#启动docker
 systemctl daemon-reload
 systemctl enable docker
 systemctl start docker
