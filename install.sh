91a578f5578cf9ed16c3ff60ca58fa935e422ec2
上天的恩赐 <19582627+stdec@users.noreply.github.com>
2021-03-16 11:37:23 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,22 +4,22 @@ yum -y install yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 yum list docker-ce.x86_64 --showduplicates | sort -r
 
-yum install -y docker-ce-19.03.5 docker-ce-cli-19.03.5 containerd.io
+yum install -y docker-ce-19.03.15 docker-ce-cli-19.03.15 containerd.io
 
 
 mkdir -p /etc/docker
 cat > /etc/docker/daemon.json <<EOF 
 {
-"insecure-registry": [
+  "insecure-registry": [
     "hub.docker.com"
-],
-"registry-mirrors": ["https://5cs233bb.mirror.aliyuncs.com"],
-"graph": "/data/docker",
-"exec-opts": ["native.cgroupdriver=systemd"]
+  ],
+  "registry-mirrors": ["https://5cs233bb.mirror.aliyuncs.com"],
+  "graph": "/data/docker",
+  "exec-opts": ["native.cgroupdriver=systemd"]
 }
 EOF
 
-mkdir -p /data/docker
+mkdir -p /data/docker | true
 
 cat >>  /etc/sysctl.conf <<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
